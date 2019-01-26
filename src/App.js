2d8d95e1f921f58aaaf6ece994c5f30bf4ba7c66
Barbara Fick <bkantzos@yahoo.com>
2019-01-25 20:27:24 -0700

    images, randomly mix tiles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Wrapper from "./components/Wrapper";
 import tiles from "./tiles.json";
 import "./App.css";
 
-
 class App extends React.Component {
 
   state = {
@@ -17,49 +16,83 @@ class App extends React.Component {
 
     const updatedTiles = tiles;
     let tileIdx = tiles.findIndex(tile => tile.id === clickedId);
-    
-    if ( this.state.tiles[tileIdx].clicked === true ) {
+
+    if (this.state.tiles[tileIdx].clicked === true) {
       console.log("WRONG.  Reset Game");
-      resetGame();
+      this.resetGame();
     } else {
       console.log("Right Answer");
-      updatedTiles[tileIdx].clicked=true;
-      this.setState({ 
+      updatedTiles[tileIdx].clicked = true;
+      this.setState({
         score: this.state.score + 1,
         tiles: updatedTiles
-     })
+      })
     }
   };
 
   resetGame = () => {
     console.log("RESETTING GAME");
-    mixTiles();
+    this.mixTiles();
     this.setState({
       score: 0
     })
   };
 
-  mixTiles = () => {
+  mixTiles = (tiles) => {
     console.log("MIX TILES");
+    return this.shuffle(tiles);
+  }
+
+  //================================================/
+  // Mix up the images inside the array 
+  // Fisher-Yates (aka Knuth) Shuffle
+  // http://sedition.com/perl/javascript-fy.html
+  //===============================================/
+  shuffle = (array) => {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+
+  componentDidMount() {
+    this.setState({
+      tiles: this.mixTiles(this.state.tiles)
+    });
   }
 
   render() {
     return (
+      <>
+      <h1 className="score">SCORE: {this.state.score}</h1>
       <Wrapper>
-        <h1 className="score">SCORE: {this.state.score}</h1>
-      { 
-          this.state.tiles.map( tile => {
-          return (
-        <ClickCard
-          key={tile.id}
-          id={tile.id} 
-          image={tile.image}
-          clicked={tile.clicked}
-          handleClick={this.tileCardClick}
-        />) } )
-      }
-       
+          {
+            this.state.tiles.map(tile => {
+              return (
+
+                <ClickCard
+                  key={tile.id}
+                  id={tile.id}
+                  image={tile.image}
+                  clicked={tile.clicked}
+                  handleClick={this.tileCardClick}
+                />)
+            })
+          }
       </Wrapper>
+      </>
     );
   }
 
--- src/components/ClickCard/index.js
+++ src/components/ClickCard/index.js
@@ -1,21 +1,13 @@
 import React from "react";
 import "./style.css";
 
-// In the 'FriendCard' component, trigger props.removeFriend and pass in the id of the friend to
-// remove onClick of the friend delete button 
-
 
 function ClickCard(props) {
 
   return (
-    <div className="card">
-      <div className="img-container">
-        <img alt={props.name} onClick={ () => props.handleClick(props.id) } src={props.image} />
+      <div className="grid-item">
+        <img alt={props.name} onClick={() => props.handleClick(props.id)} src={props.image} />
       </div>
-      <div className="content">
-
-      </div>
-    </div>
   );
 }
 
